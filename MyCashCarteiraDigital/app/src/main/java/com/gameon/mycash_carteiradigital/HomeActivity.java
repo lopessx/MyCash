b4203247cad07021064b9bde43cc27677da18631
Victor Falcão <victorfalcao33@gmail.com>
2020-07-05 21:21:59 -0300

    Tela Home

---
+++ MyCashCarteiraDigital/app/src/main/java/com/gameon/mycash_carteiradigital/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.gameon.mycash_carteiradigital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_home);
+    }
+}
